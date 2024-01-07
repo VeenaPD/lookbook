5be5f355e525d23b7219d7b6b6a932c0f70ef6ab
VeenaPD <veenapd4@gmail.com>
2024-01-07 21:59:23 +0530

    add gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { SharedService } from './core/services/shared.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -8,11 +12,26 @@ import { SharedService } from './core/services/shared.service';
 })
 export class AppComponent implements OnInit {
   data: any = [];
-  constructor(private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService,
+    private router: Router,
+    @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
     this.sharedService.getSharedData().subscribe(data => {
       this.data = data;
+    });
+    this.handleRouteEvents();
+  }
+
+  handleRouteEvents() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('event', 'page_view', {
+          page_title: event.urlAfterRedirects === "/" ? "Home" : event.urlAfterRedirects.slice(1),
+          page_path: event.urlAfterRedirects,
+          page_location: this.document.location.href
+        })
+      }
     });
   }
 
